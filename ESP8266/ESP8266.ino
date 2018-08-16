3206bc4f2f15f37b71e665059758ad4aca54e2fa
Massimo <massimop1973@gmail.com>
2018-08-16 18:39:05 +0200

    improve program

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -1,45 +1,66 @@
 #include <CayenneMQTTESP8266.h>
 #include <DHT.h>
 #include <LedControl.h>
+
 #define CAYENNE_PRINT Serial
-#define dht_pin D2
-#define dht_type DHT11
+
+#define button D1
+#define led D2
 
 char ssid[] = "NETGEAR";
 char wifiPassword[] = "";
 char username[] = "0119e290-98e0-11e8-ab4b-11d2d0c54106";
 char password[] = "99a7436f4b2ea5865a1076f52ce647544cf55e14";
-char clientID[] = "50094dd0-9e41-11e8-9300-637b1fa64cc6";
+char clientID[] = "36795510-a15a-11e8-a59a-6d22310ed7cb";
 
 unsigned long seconds;
-float temp;
-float hum;
-int din = 16; // Pin corrispondente D0
+int buttonPressed = 0;
+long wifi_strength;
+int UVsensorIn = A0; //Output from the sensor
 
-int CS =  5;  // D1
-
-int CLK = 4;  // D2
-
-DHT dht(dht_pin, dht_type);
-
-void setup() {
+void setup()
+{
+	pinMode(led, OUTPUT);
+	pinMode(button, INPUT_PULLUP);
+	pinMode(UVsensorIn, INPUT);
 	Serial.begin(9600);
 	Cayenne.begin(username, password, clientID, ssid, wifiPassword);
-	dht.begin();
+	attachInterrupt(digitalPinToInterrupt(button), interruptPulsante, FALLING);
 }
 
-void loop() {
-	seconds=millis();
-	hum = dht.readHumidity();
-    temp= dht.readTemperature();
+void loop()
+{
+	seconds = millis();
+	if (buttonPressed)
+	{
+		digitalWrite(led, HIGH);
+	}
+	else
+	{
+		digitalWrite(led, LOW);
+	}
+	wifi_strength = WiFi.RSSI();
+
+	int uvLevel = averageAnalogRead(UVsensorIn);
+
+	float outputVoltage = 3.3 * uvLevel / 1024;
+	float uvIntensity = mapfloat(outputVoltage, 0.99, 2.9, 0.0, 15.0);
+
+	Serial.print(" UV Intensity: ");
+	Serial.print(uvIntensity);
+	Serial.print(" mW/cm^2");
+	Serial.println();
+
 	Cayenne.loop();
 }
 
 CAYENNE_OUT_DEFAULT()
 {
 	Cayenne.virtualWrite(0, seconds);
-	Cayenne.celsiusWrite(1, temp);
-	Cayenne.celsiusWrite(2, hum);
+	Cayenne.luxWrite(1, wifi_strength);
+	Cayenne.virtualWrite(2, buttonPressed);
+
+	// Cayenne.celsiusWrite(2, hum);
 	//Cayenne.luxWrite(2, 700);
 	//Cayenne.virtualWrite(3, 50, TYPE_PROXIMITY, UNIT_CENTIMETER);
 }
@@ -47,13 +68,32 @@ CAYENNE_OUT_DEFAULT()
 CAYENNE_IN_DEFAULT()
 {
 	CAYENNE_LOG("Channel %u, value %s", request.channel, getValue.asString());
-	switch (request.channel) {
-		case 3: // button
-			Serial.print("button");
-			Serial.println(getValue.asString());
-			break;
-		case 4: // fade
-			Serial.print("fade");
-			Serial.println(getValue.asString());
+	switch (request.channel)
+	{
+	case 3:
+		buttonPressed = 0;
+		break;
 	}
+}
+
+void interruptPulsante()
+{
+	buttonPressed = 1;
+}
+
+int averageAnalogRead(int pinToRead)
+{
+	byte numberOfReadings = 8;
+	unsigned int runningValue = 0;
+
+	for (int x = 0; x < numberOfReadings; x++)
+		runningValue += analogRead(pinToRead);
+	runningValue /= numberOfReadings;
+
+	return (runningValue);
+}
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
